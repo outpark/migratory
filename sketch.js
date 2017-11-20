ef8e16725d08b380a1d9299e8d8af15aa0c8babe
Seongsoon Park <sp3005@nyu.edu>
2017-11-19 21:52:28 -0500

    init commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var world;
+
+function setup() {
+	// no canvas needed
+	noCanvas();
+
+	world = new World('VRScene');
+	world.setUserPosition(0, 0, 50);
+}
+
+function draw() {
+	
+}
+
+function setMountains() {
+
+}
+
+function setSky() {
+
+}
+
