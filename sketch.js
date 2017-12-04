aff7c0adde8fad0ce75936f1b399f5274695c995
Seongsoon Park <sp3005@nyu.edu>
2017-12-04 13:21:19 -0500

    progressive schene generation

--- sketch.js
+++ sketch.js
@@ -9,8 +9,15 @@ var cone;
 var obstacles = [];
 
 var cloudCount = 10;
-var moutainCount = 5;
-var treeCount = 20;
+var moutainCount = 7;
+var treeCount = 25;
+
+
+var treeContainer;
+var mountainContainer;
+var cloudContainer;
+
+var changeScene = 0
 
 // questions to ask: how to remove from world?
 // how to smartly do collision for each object
@@ -40,10 +47,16 @@ function setup() {
 		z:0
 	});
 	setGround();
-	setMountains();
-	setTree();
+	bird = world.getUserPosition();
+
+	setMountains(mountainContainer, bird);
+	setTrees(treeContainer,bird);
 	setPyramid();
-	setClouds();
+	setClouds(cloudContainer, bird);
+
+	world.add(treeContainer);
+	world.add(mountainContainer);
+	world.add(cloudContainer);
 	//adding 3d model for cloud1
 	
 }
@@ -58,28 +71,48 @@ function movementCtrl(){
 	// if(!bird.y < 2)
 		world.moveUserForward(0.1);
 
+	// if(bird.z < -30){
+	// 	// world.remove(treeContainer);
+	// 	treeContainer.hide();
+	// }
+
 	bird = world.getUserPosition();
 	circle.setX(bird.x);
 	circle.setZ(bird.z);
 	console.log(circle.getWorldPosition());
 
-	if(Math.floor(bird.z % 100) === 0){
+	console.log(bird.z % 50);
+	if(-Math.floor(bird.z % 50) === 49 && changeScene < 1){
 		// change environment every 100 movement
+		// debugger;
+		emptyContainer(treeContainer);
+		emptyContainer(mountainContainer);
+		emptyContainer(cloudContainer);
+		setTrees(treeContainer, bird);
+		setMountains(mountainContainer, bird);
+		setClouds(cloudContainer, bird);
+		console.log("Changing!!");
+		changeScene++;
+	}
+
+	if(-Math.floor(bird.z % 50) === 1){
+		changeScene = 0;
 	}
 }
 
-function setClouds() {
+function setClouds(container, bird) {
 	for(let i=0;i<cloudCount;i++){
 		cloud = new OBJ({
 			asset: 'cloud1_obj',
 			mtl: 'cloud1_mtl',
 			x: random(-30,30),
 			y: random(3,30),
-			z: random(-10,-60),
+			z: random(bird.z,bird.z-60),
 			rotationX:0,
 			rotationY:0
 		});
-		world.add(cloud);
+		// world.add(cloud);
+		container.addChild(cloud);
 	}
 }
 
@@ -88,27 +121,28 @@ function setGround() {
 	world.add(circle);
 }
 
-function setMountains() {
-	for(let i=0;i<5;i++){
+function setMountains(container, bird) {
+	for(let i=0;i<moutainCount;i++){
 		var r= random(255);
 		var g= random(255);
 		var b = random(255);
 		var xPos = random(-40,40);
-		var zPos = random(-10, -90);
+		var zPos = random(bird.z-20, bird.z-80);
 		newCone = new Cone({
 			x:xPos, y:4, z:zPos,
 			red:r, green:g, blue:b,
 			height:30,
 			radiusBottom:15, radiusTop:0.01,
 		});
-		world.add(newCone);
+		// world.add(newCone);
+		container.addChild(newCone);
 	}
 }
 
-function setTree() {
-	for(let i=0;i<20;i++){
+function setTrees(container, bird) {
+	for(let i=0;i<treeCount;i++){
 		let xPos = random(-30,30);
-		let zPos = random(-5, -40)
+		let zPos = random(bird.z, bird.z-60)
 		let height = random(5, 10);
 		var trunk = new Cylinder({
 		x:xPos, y:1 , z:zPos,
@@ -116,14 +150,16 @@ function setTree() {
 		radius: 0.25,
 		red: 83, green: 53, blue:10,
 		});
-		world.add(trunk);
+		// world.add(trunk);
 		var leaves = new Cone({
 			x:xPos, y:5, z:zPos,
 			height:height,
 			radiusBottom:1, radiusTop:0.01,
 			red:30, green:147, blue: 45
 		});
-		world.add(leaves);
+		// world.add(leaves);
+		container.addChild(trunk);
+		container.addChild(leaves);
 	}
 }
 
@@ -144,6 +180,13 @@ function setPyramid(){
 
 	world.add(base);
 
+}
+
+function emptyContainer(container){
+	let entities = container.getChildren();
+	entities.forEach((entity) => {
+		container.removeChild(entity);
+	});
 }
 
 
